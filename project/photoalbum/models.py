992d6d98be31a64937e1576d237d64f62faf6414
LordPatriot <orekhov.volodya@gmail.com>
2014-01-12 22:16:08 +0200

    Models are described
    This commit contains models.py, it will be tested in views.py (not in
    tests.py for some reason) as soon as we will merge commits with Peter’s
    authentication.

--- project/photoalbum/models.py
+++ project/photoalbum/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    mail = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    #any additional things we want to know about users
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    owner = models.ForeignKey('User')
+
+class Slide(models.Model):
+    order_number = models.IntegerField(default=0)
+    album = models.ForeignKey('Album')
+
+class Photo(models.Model):
+    link = models.URLField(max_length=255)
+    slide = models.ForeignKey('Slide')
+
+class Order(models.Model):
+    time_placed = models.TimeField()
+    album = models.ForeignKey('Album')
+    owner = models.ForeignKey('User')
