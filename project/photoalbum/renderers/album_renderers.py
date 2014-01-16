ce1daae9b0da3e6f94cd844caaf6f0cd38083a4c
LordPatriot <orekhov.volodya@gmail.com>
2014-01-16 22:32:53 +0200

    Contains a better album listing and adds directories to project.pyproj

--- project/photoalbum/renderers/album_renderers.py
+++ project/photoalbum/renderers/album_renderers.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from photoalbum.models import Album
+from photoalbum.models import Album, Slide
 
 def album_list_view(request):
+    for i in range (0, 5):
+        Album.objects.create(name=("Album " + str(i)), owner=request.user)
+
     albums = Album.objects.filter(owner=request.user)
+    #for a in albums:
+    #    slides = Slide.objects.filter(album=a)
+    #    a.slides_num = len(slides)
     return render_to_response("album_list.html", {"albums": albums})
 
 def album_view(request, album_id):
