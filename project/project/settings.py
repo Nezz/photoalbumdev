050068301b51cf77d47d77a32795d38fd7988a6e
Nezz <bloodsource@vipmail.hu>
2014-01-19 15:14:40 +0200

    Collect static files into solution root to be served using azure

--- project/project/settings.py
+++ project/project/settings.py
@@ -92,4 +92,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'static'))
+
 STATIC_URL = '/static/'
