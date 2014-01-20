aab5bf614c50dc1689f9c81023ccd794005161db
Nezz <bloodsource@vipmail.hu>
2014-01-20 20:07:52 +0200

    Added bootbox to handle bootstrap modals in the feature seamlessly Use RequestContext when rendering (provides additional variables like user or request) Basic bootstrap template and navbar with inheritence Added navactive context processor to handle active navbar items automagically

--- project/photoalbum/renderers/album_renderers.py
+++ project/photoalbum/renderers/album_renderers.py
@@ -2,13 +2,14 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.core.context_processors import csrf
 from django.core.exceptions import ObjectDoesNotExist
+from django.template import RequestContext
 from photoalbum.models import Album, Slide, Photo
 
 def album_list_view(request):
     albums = Album.objects.filter(owner=request.user)
     c = {"albums": albums}
     c.update(csrf(request))
-    return render_to_response("album_list.html", c)
+    return render_to_response("album_list.html", RequestContext(request, c))
 
 def album_view(request, album_id, slide_id=1):
     slide_id = int(slide_id)
@@ -36,7 +37,5 @@ def album_view(request, album_id, slide_id=1):
 
     c = {"album": album, "curr" : curr, "next" : next, "prev" : prev, "photos" : photos, "editable" : editable}
     c.update(csrf(request))
-    if (album.owner == request.user):
-        return render_to_response("album.html", c) # TODO: Album editor
-    else:
-        return render_to_response("album.html", c) # TODO: Album viewer
+
+    return render_to_response("album.html", RequestContext(request, c))
