603db2d7779881c7ff8417b64028853f2a53dff6
LordPatriot <orekhov.volodya@gmail.com>
2014-01-20 15:25:11 +0200

    Order functionality added

---
+++ project/photoalbum/renderers/order_renderers.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render_to_response
+from photoalbum.models import Order, Album
+from django.core.context_processors import csrf
+import datetime
+
+now = datetime.datetime.now()
+
+def order_list_view(request):
+    orders = Order.objects.filter(owner=request.user)
+    c = {"orders": orders}
+    c.update(csrf(request))
+    return render_to_response("order_list.html", c)
+
+def order_create_view(request, album_id):
+    Order.objects.create(owner=request.user, album=Album.objects.get(guid=album_id), time_placed=datetime.datetime.now())
+
+    orders = Order.objects.filter(owner=request.user)
+    c = {"orders": orders}
+    c.update(csrf(request))
+    return render_to_response("order_list.html", c)
